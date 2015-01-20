f2fa072dbea1f9a6d80c28a22913f865a455e2d3
taiga0213 <kuronosx@yahoo.co.jp>
2015-01-20 21:26:20 +0900

    [追加] タイムラインの新着更新 タイムラインのさかのぼり

--- app/src/main/java/jp/taiga0213/twittertest/TweetListActivity.java
+++ app/src/main/java/jp/taiga0213/twittertest/TweetListActivity.java
@@ -1,18 +1,20 @@
 package jp.taiga0213.twittertest;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
@@ -22,8 +24,10 @@ import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Search;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.SearchService;
+import com.twitter.sdk.android.core.services.StatusesService;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 import com.twitter.sdk.android.tweetui.CompactTweetView;
+import com.twitter.sdk.android.tweetui.LoadCallback;
 import com.twitter.sdk.android.tweetui.TweetViewFetchAdapter;
 
 import java.util.ArrayList;
@@ -41,41 +45,95 @@ public class TweetListActivity extends ActionBarActivity implements View.OnClick
 
     TwitterApiClient twitterApiClient;
 
-    ListView listView;
+    PullToRefreshListView listView;
 
     private static final int SEARCH_COUNT = 100;
     private static final String SEARCH_RESULT_TYPE = "popular";
     public static final int MENU_LOGOUT = 0;
     public static final int MENU_TWEET = 1;
 
+    private static long NEWEST_TWEET_ID;
+    private static long OLDEST_TWEET_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_list);
 
-        listView = (ListView) findViewById(R.id.listView);
+        listView = (PullToRefreshListView) findViewById(R.id.listView);
         twitterApiClient = TwitterCore.getInstance().getApiClient();
 
         Button searchButton = (Button) findViewById(R.id.search);
         searchButton.setOnClickListener(this);
 
+        NEWEST_TWEET_ID = Long.MIN_VALUE;
+        OLDEST_TWEET_ID = Long.MAX_VALUE;
 
 
+        StatusesService statusesService = twitterApiClient.getStatusesService();
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        statusesService.homeTimeline(10, null, null, false, false, false,
+                false, new Callback<List<Tweet>>() {
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        // TODO 自動生成されたメソッド・スタブ
+                        Log.e("error", "miss");
+                        exception.printStackTrace();
+
+                    }
+
+                    @Override
+                    public void success(Result<List<Tweet>> result) {
+                        // TODO 自動生成されたメソッド・スタブ
+                        for (Tweet tweet : result.data) {
+                            tweetIds.add(tweet.id);
+                            Log.d("id", String.valueOf(tweet.id));
+
+                            if (NEWEST_TWEET_ID < tweet.id)
+                                NEWEST_TWEET_ID = tweet.id;
+
+                            if (OLDEST_TWEET_ID > tweet.id)
+                                OLDEST_TWEET_ID = tweet.id;
+                        }
+
+                        Log.d("ID:NEW", String.valueOf(NEWEST_TWEET_ID));
+                        Log.d("ID:OLD", String.valueOf(OLDEST_TWEET_ID));
+
+                        adapter.setTweetIds(tweetIds,
+                                new LoadCallback<List<Tweet>>() {
+                                    @Override
+                                    public void success(List<Tweet> tweets) {
+                                        // my custom actions
+                                    }
+
+                                    @Override
+                                    public void failure(TwitterException exception) {
+                                        // Toast.makeText(...).show();
+                                        exception.printStackTrace();
+                                    }
+                                });
+
+                    }
+                });
+        listView.setAdapter(adapter);
+
+        listView.setMode(PullToRefreshBase.Mode.BOTH);
+
+        listView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                ListView tweetList = (ListView) parent;
-                Tweet tweet = (Tweet) tweetList.getItemAtPosition(position);
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //引き下げ(上);
+                new NewTweetAdd().execute();
+            }
 
-                Intent intent = new Intent(getApplicationContext(), SingleTweetActivity.class);
-                intent.putExtra("id", tweet.id);
-                startActivity(intent);
-
-                return false;
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                //引き上げ(下)
+                new OldTweetAdd().execute();
             }
         });
+
     }
 
     @Override
@@ -152,6 +210,8 @@ public class TweetListActivity extends ActionBarActivity implements View.OnClick
 
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
+
+                finish();
                 return true;
             case MENU_TWEET:
                 TweetComposer.Builder builder = new TweetComposer.Builder(this);
@@ -161,5 +221,119 @@ public class TweetListActivity extends ActionBarActivity implements View.OnClick
 
         }
         return false;
+    }
+
+
+    private class OldTweetAdd extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            StatusesService statusesService = twitterApiClient.getStatusesService();
+
+            statusesService.homeTimeline(20, null, OLDEST_TWEET_ID + 1, false, false, false,
+                    false, new Callback<List<Tweet>>() {
+
+                        @Override
+                        public void failure(TwitterException exception) {
+                            // TODO 自動生成されたメソッド・スタブ
+                            Log.e("error", "miss");
+
+                        }
+
+                        @Override
+                        public void success(Result<List<Tweet>> result) {
+                            // TODO 自動生成されたメソッド・スタブ
+                            for (Tweet tweet : result.data) {
+                                tweetIds.add(tweet.id);
+                                Log.d("id", String.valueOf(tweet.id));
+
+                                if (OLDEST_TWEET_ID > tweet.id)
+                                    OLDEST_TWEET_ID = tweet.id;
+
+                            }
+
+                            Log.d("ID:NEW", String.valueOf(NEWEST_TWEET_ID));
+                            Log.d("ID:OLD", String.valueOf(OLDEST_TWEET_ID));
+
+
+                            adapter.setTweetIds(tweetIds,
+                                    new LoadCallback<List<Tweet>>() {
+                                        @Override
+                                        public void success(List<Tweet> tweets) {
+                                            // my custom actions
+                                        }
+
+                                        @Override
+                                        public void failure(TwitterException exception) {
+                                            // Toast.makeText(...).show();
+                                            exception.printStackTrace();
+                                        }
+                                    });
+
+                        }
+                    });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            listView.onRefreshComplete();//更新アニメーション終了
+        }
+    }
+
+    private class NewTweetAdd extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            StatusesService statusesService = twitterApiClient.getStatusesService();
+
+            statusesService.homeTimeline(100, NEWEST_TWEET_ID, null, false, false, false,
+                    false, new Callback<List<Tweet>>() {
+
+                        @Override
+                        public void failure(TwitterException exception) {
+                            // TODO 自動生成されたメソッド・スタブ
+                            Log.e("error", "miss");
+
+                        }
+
+                        @Override
+                        public void success(Result<List<Tweet>> result) {
+                            // TODO 自動生成されたメソッド・スタブ
+                            for (Tweet tweet : result.data) {
+                                tweetIds.add(0, tweet.id);
+                                Log.d("id", String.valueOf(tweet.id));
+
+                                if (NEWEST_TWEET_ID < tweet.id)
+                                    NEWEST_TWEET_ID = tweet.id;
+
+                            }
+
+                            Log.d("ID:NEW", String.valueOf(NEWEST_TWEET_ID));
+                            Log.d("ID:OLD", String.valueOf(OLDEST_TWEET_ID));
+
+                            adapter.setTweetIds(tweetIds,
+                                    new LoadCallback<List<Tweet>>() {
+                                        @Override
+                                        public void success(List<Tweet> tweets) {
+                                            // my custom actions
+                                        }
+
+                                        @Override
+                                        public void failure(TwitterException exception) {
+                                            // Toast.makeText(...).show();
+                                            exception.printStackTrace();
+                                        }
+                                    });
+
+                            adapter.notifyDataSetChanged();
+
+                        }
+                    });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            listView.onRefreshComplete();//更新アニメーション終了
+        }
     }
 }
